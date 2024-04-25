dc450ada0bbbce72f8be4791e8811c29ea0947e1
Ivanich <deniska090798@gmail.com>
2024-04-25 21:49:22 +0400

    first commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    //java -jar artifacts/aqa-shop.jar
+    //docker compose up
+}
