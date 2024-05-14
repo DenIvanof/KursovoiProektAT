9faaf9628f66f43a27c9f967502b1aacfa1a1780
Ivanich <deniska090798@gmail.com>
2024-05-14 23:22:44 +0400

    fix

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,4 +1,5 @@
 package ru.netology.data;
+
 import com.github.javafaker.Faker;
 import lombok.Value;
 
@@ -11,136 +12,185 @@ import static java.lang.String.format;
 public class DataHelper {
     private DataHelper() {
     }
+
     public static final Faker faker = new Faker(new Locale("en"));
+
     @Value
     public static class CardInfo {
-        String cardnumber;
+        String cardNumber;
         String month;
         String year;
         String holder;
-        String codcvccvv;
+        String codCvcCvv;
     }
+
     //Номер карты
     //Валидные значения
     public static String getApprovedCardNumber() {
         return ("1111 2222 3333 4444");
     }
+
     public static String getDeclinedCardNumber() {
         return ("5555 6666 7777 8888");
     }
+
     //Невалидные значения
     public static CardInfo getCardNumberEmpty() {
         return new CardInfo("", getValidMonth(), getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getCardNumberSmall() {
         return new CardInfo("1111 2222 3333 44", getValidMonth(), getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getCardNumberCyrillic() {
         return new CardInfo("1111 2222 3333 44гг", getValidMonth(), getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getCardNumberLatin() {
         return new CardInfo("1111 2222 3333 44ff", getValidMonth(), getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getCardNumberSymbol() {
         return new CardInfo("1111 2222 3333 44@&", getValidMonth(), getValidYear(), getValidHolder(), getValidCVC());
     }
+
     //Месяц
     //Валидные значения
     public static String getValidMonth() {
         LocalDate localDate = LocalDate.now();
         return format("%02d", localDate.getMonthValue());
     }
+
     //Невалидные значения
     public static CardInfo getMonthEmpty() {
         return new CardInfo(getApprovedCardNumber(), "", getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getMonth13() {
         return new CardInfo(getApprovedCardNumber(), "13", getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getMonthSymbol() {
         return new CardInfo(getApprovedCardNumber(), "1!", getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getMonthLatin() {
         return new CardInfo(getApprovedCardNumber(), "f2", getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getMonthCyrillic() {
         return new CardInfo(getApprovedCardNumber(), "г2", getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getMonthOneDigit() {
         return new CardInfo(getApprovedCardNumber(), "0", getValidYear(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getMonthTwoZeros() {
         return new CardInfo(getApprovedCardNumber(), "00", getValidYear(), getValidHolder(), getValidCVC());
     }
+
     //Год
     //Валидные значения
     public static String getValidYear() {
-        return LocalDate.now().plusYears(0).format(DateTimeFormatter.ofPattern("yy"));
+        int year = 0;
+        return LocalDate.now().plusYears(year).format(DateTimeFormatter.ofPattern("yy"));
     }
+
     //Невалидные значения
+    public static String getYearLess() {
+        int year = -1;
+        return LocalDate.now().plusYears(year).format(DateTimeFormatter.ofPattern("yy"));
+    }
+
+    public static String getYearMore() {
+        int year = 10;
+        return LocalDate.now().plusYears(year).format(DateTimeFormatter.ofPattern("yy"));
+    }
+
     public static CardInfo getYearLessThanCurrent() {
-        return new CardInfo(getApprovedCardNumber(), getValidMonth(), "23", getValidHolder(), getValidCVC());
+        return new CardInfo(getApprovedCardNumber(), getValidMonth(), getYearLess(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getYearMoreThanTheCurrentOne() {
-        return new CardInfo(getApprovedCardNumber(), getValidMonth(), "34", getValidHolder(), getValidCVC());
+        return new CardInfo(getApprovedCardNumber(), getValidMonth(), getYearMore(), getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getYearSymbol() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), "2%", getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getYearLatin() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), "qw", getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getYearCyrillic() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), "цй", getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getYearOneDigit() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), "2", getValidHolder(), getValidCVC());
     }
+
     public static CardInfo getYearEmpty() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), "", getValidHolder(), getValidCVC());
     }
+
     //Владелец
     //Валидные значения
     public static String getValidHolder() {
         return faker.name().firstName();
     }
+
     //Невалидные значения
     public static CardInfo getHolderCyrillic() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), "Денис", getValidCVC());
     }
+
     public static CardInfo getHolderSymbol() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), "%denis", getValidCVC());
     }
+
     public static CardInfo getHolderDigit() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), "1denis", getValidCVC());
     }
+
     public static CardInfo getHolderOneLetter() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), "S", getValidCVC());
     }
+
     public static CardInfo getHolderMoreThan100() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), "Aaaaaaaaaaaaaaaaaaaaaaaaadddddddddddddddddddddddddlllllllllllllllllllllllllyyyyyyyyyyyyyyyyyyyyyyyyyeeeeeeeeee", getValidCVC());
     }
+
     public static CardInfo getHolderEmpty() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), "", getValidCVC());
     }
+
     //CVC/CVV
     //Валидные значения
     public static String getValidCVC() {
         return ("123");
     }
+
     //Невалидные значения
     public static CardInfo getCVCSymbol() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), getValidHolder(), "12#");
     }
+
     public static CardInfo getCVCLatin() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), getValidHolder(), "22r");
     }
+
     public static CardInfo getCVCCyrillic() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), getValidHolder(), "22r");
     }
+
     public static CardInfo getCVCtwoDigit() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), getValidHolder(), "22");
     }
+
     public static CardInfo getCVCempty() {
         return new CardInfo(getApprovedCardNumber(), getValidMonth(), getValidYear(), getValidHolder(), "");
     }
--- src/test/java/ru/netology/page/PurchasePage.java
+++ src/test/java/ru/netology/page/PurchasePage.java
@@ -13,31 +13,32 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class PurchasePage {
     //Начальная страница
-    private final SelenideElement Buy = $(byText("Купить"));
-    private final SelenideElement Buyaloan = $(byText("Купить в кредит"));
-    private final SelenideElement Paymentcard = $(byText("Оплата по карте"));
-    private final SelenideElement Creditcard = $(byText("Кредит по данным карты"));
+    private final SelenideElement buy = $(byText("Купить"));
+    private final SelenideElement buyAloan = $(byText("Купить в кредит"));
+    private final SelenideElement paymentCard = $(byText("Оплата по карте"));
+    private final SelenideElement creditCard = $(byText("Кредит по данным карты"));
 
     //Оплата по карте
-    public DashboardPage BuyCard() {
-        Buy.click();
-        Paymentcard.shouldBe(visible);
-        return new DashboardPage();
+    public PurchasePage buyCard() {
+        buy.click();
+        paymentCard.shouldBe(visible);
+        return new PurchasePage();
     }
+
     //Покупка в кредит
-    public DashboardPage BuyCreditCard() {
-        Buyaloan.click();
-        Creditcard.shouldBe(visible);
-        return new DashboardPage();
+    public PurchasePage buyCreditCard() {
+        buyAloan.click();
+        creditCard.shouldBe(visible);
+        return new PurchasePage();
     }
 
     //Поля формы
-    private SelenideElement cardnumber = $("[placeholder='0000 0000 0000 0000']");
+    private SelenideElement cardNumber = $("[placeholder='0000 0000 0000 0000']");
     private SelenideElement month = $(byText("Месяц")).parent().$(".input__control");
     private SelenideElement year = $(byText("Год")).parent().$(".input__control");
     private SelenideElement holder = $(byText("Владелец")).parent().$(".input__control");
-    private SelenideElement cvccvv = $(byText("CVC/CVV")).parent().$(".input__control");
-    private SelenideElement continuebutton = $(byText("Продолжить"));
+    private SelenideElement cvcCvv = $(byText("CVC/CVV")).parent().$(".input__control");
+    private SelenideElement continueButton = $(byText("Продолжить"));
 
     //Ошибки
     private SelenideElement formatCardError = $(byText("Неверный формат")).parent().$(".input__sub");
@@ -51,48 +52,57 @@ public class PurchasePage {
 
     //Заполненная форма
     public void fillingOutTheForm(DataHelper.CardInfo cardInfo) {
-        cardnumber.setValue(cardInfo.getCardnumber());
+        cardNumber.setValue(cardInfo.getCardNumber());
         month.setValue(cardInfo.getMonth());
         year.setValue(cardInfo.getYear());
         holder.setValue(cardInfo.getHolder());
-        cvccvv.setValue(cardInfo.getCodcvccvv());
-        continuebutton.click();
+        cvcCvv.setValue(cardInfo.getCodCvcCvv());
+        continueButton.click();
     }
+
     //Не заполненная форма
-    public void emptyform() {
-        continuebutton.click();
+    public void emptyForm() {
+        continueButton.click();
         cardNumberError.shouldBe(visible);
         monthError.shouldBe(visible);
         yearError.shouldBe(visible);
         holderError.shouldBe(visible);
         cvcError.shouldBe(visible);
     }
+
     //Оплата
-    public void paymentSuccessfull() {
+    public void paymentSuccessFull() {
         $(".notification_status_ok").shouldBe(Condition.visible, Duration.ofSeconds(30));
     }
 
     public void declinedPayment() {
         $(byCssSelector("div.notification.notification_status_error.notification_has-closer.notification_stick-to_right.notification_theme_alfa-on-white")).shouldBe(Condition.visible, Duration.ofSeconds(20));
     }
+
     public void invalidCardFormat() {
         formatCardError.shouldBe(visible);
     }
+
     public void invalidMonthFormat() {
         monthError.shouldBe(visible);
     }
+
     public void invalidCardExpirationDate() {
         expiredCardError.shouldBe(visible);
     }
+
     public void invalidYearFormat() {
         yearError.shouldBe(visible);
     }
-    public void theСardExpired() {
+
+    public void theCardExpired() {
         termCardError.shouldBe(visible);
     }
+
     public void invalidHolderFormat() {
         holderError.shouldBe(visible);
     }
+
     public void invalidCvcFormat() {
         cvcError.shouldBe(visible);
     }
